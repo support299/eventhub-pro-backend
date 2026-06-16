ce2cccde1e8dbe8f6b3d41118dfe172425ed61f2
Muhammed Mijwad <mijuzz007@gmail.com>
2026-06-16 17:58:15 +0300

    Update URL path for Django admin from "admin/" to "django-admin/" in urls.py for improved clarity and consistency.

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("django-admin/", admin.site.urls),
     path("api/", include("accounts.urls")),
     path("api/", include("events.urls")),
     path("api/", include("ghl.urls")),
